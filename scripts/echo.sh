85557526ce7d8e95b95903a95b1e32642a6d7339
Kostis Tsaprailis <kostis.tsaprailis@gmail.com>
2017-07-17 16:49:17 +0300

    Adding python script

--- scripts/echo.sh
+++ scripts/echo.sh
@@ -6,5 +6,17 @@ ctx logger info "current working directory is: ${PWD}"
 # will log "first arg is: arg1_value"
 ctx logger info "first arg is: $1"
 
+# will log "first arg is: arg1_value"
+ctx logger info "second arg is: $2"
+
 # will log "my env variable is: MY_ENV_VARIABLE_VALUE"
 ctx logger info "my env variable is: ${MY_ENV_VARIABLE}"
+
+# Running a specific script contained on the machine using a parameter passed 
+#./runPsScript.sh $1
+
+# This command retrieves the machine tags
+# They are returned as a list of dicts
+tags=`ctx instance runtime-properties info.tags`
+# Then we are logging them.
+ctx logger info "tags: $tags"
---
+++ scripts/logger.py
@@ -0,0 +1,30 @@
+import subprocess
+from cloudify import ctx
+from cloudify.state import ctx_parameters as inputs
+
+# This logs the configure lifecycle script params.
+# These are passed from the script_params input.
+args = inputs['process']['args']
+ctx.logger.info("The script args are: {0}".format(args))
+
+# This line retrieves the machine tags
+tags = ctx.instance.runtime_properties['info']['tags']
+ctx.logger.info("tags are: {0}".format(tags))
+
+# Iterate over all machine tags
+for tag in tags:
+    # get the key of the tag
+    if tag['key'] == 'prod':
+        ctx.logger.info("This machine is in prod!")
+        ctx.logger.info("We have the following argument: {0}".format(args[0]))
+        # This is how a shell command would be run
+        # command = "runPsScript.sh " + args[0]
+        command = "echo  " + args[0]
+        response = subprocess.call(command, shell=True)
+        if response != 0:
+            ctx.logger.info("There was an error while running the script.")
+        else:
+            ctx.logger.info("Script has run successfully.")
+    else:
+        ctx.logger.info("This machine is not production critical!")
+        ctx.logger.info("We have the following argument: {0}".format(args[0]))
